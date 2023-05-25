b2edd61d10d7ee4fcfa6e7358527cb08090ebbb8
Emma Walsh <emmaharte33@outlook.com>
2023-05-25 21:49:49 +0100

    Add helper comments to glsl file

--- src/shaders/test/vertex.glsl
+++ src/shaders/test/vertex.glsl
@@ -3,7 +3,9 @@ uniform mat4 viewMatrix;
 uniform mat4 modelMatrix;
 attribute vec3 position;
 
-void main()
+void main() // void if returns no value type, otherwise float, int etc
 {
+    float a = 1.0; // typed language, int, float, bool, vec2, vec3, vec4
+
     gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
 }
